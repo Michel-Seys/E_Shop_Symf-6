de5db95e7e85fbc95b95bdcbea314c93c39cf9d6
Derwych <seysmich@gmail.com>
2023-09-18 09:20:00 +0200

    Product Add & Edit Form

--- src/Controller/Admin/ProductsController.php
+++ src/Controller/Admin/ProductsController.php
@@ -3,9 +3,13 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Products;
+use App\Form\ProductsFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 #[Route('/admin/products', name: 'admin_products_')]
 /**
@@ -28,10 +32,37 @@ class ProductsController extends AbstractController
      * Summary of index
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function add(): Response
+    public function add(Request $request, EntityManagerInterface $em, SluggerInterface $slugger): Response
     {
-        $this->denyAccessUnlessGranted('ROLE_PRODUCT_ADMIN');
-        return $this->render('admin/products/index.html.twig');
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+        //Creation nouveau produit
+        $product = new Products();
+
+        //Creation formulaire
+        $productForm = $this->createForm(ProductsFormType::class, $product);
+
+        //Traitement de la requete
+        $productForm->handleRequest($request);
+        if($productForm->isSubmitted() && $productForm->isValid())
+        {
+            $slug = $slugger->slug($product->getName());
+            $product->setSlug($slug);
+            $prix= $product->getPrice() * 100;
+            $product->setPrice($prix);
+            
+            $em->persist($product);
+            $em->flush();
+
+            $this->addFlash('success', 'Produit ajouté !');
+
+            $this->redirectToRoute('admin_products_index');
+
+        }
+
+        return $this->render('admin/products/add.html.twig', [
+            'productForm' => $productForm->createView()
+        ]);
     }
 
     #[Route('/edit/{id}', name: 'edit')]
@@ -39,10 +70,38 @@ class ProductsController extends AbstractController
      * Summary of index
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function edit(Products $product): Response
+    public function edit(Products $product, Request $request, EntityManagerInterface $em, SluggerInterface $slugger): Response
     {
         $this->denyAccessUnlessGranted('PRODUCT_EDIT', $product);
-        return $this->render('admin/products/index.html.twig');
+
+        $prix= $product->getPrice() / 100;
+        $product->setPrice($prix);
+        //Creation formulaire
+        $productForm = $this->createForm(ProductsFormType::class, $product);
+
+        //Traitement de la requete
+        $productForm->handleRequest($request);
+        if($productForm->isSubmitted() && $productForm->isValid())
+        {
+            $slug = $slugger->slug($product->getName());
+            $product->setSlug($slug);
+            
+            $prix= $product->getPrice() * 100;
+            $product->setPrice($prix);
+            
+            $em->persist($product);
+            $em->flush();
+
+            $this->addFlash('success', 'Produit modifié !');
+
+            $this->redirectToRoute('admin_products_index');
+
+        }
+
+        return $this->render('admin/products/edit.html.twig', [
+            'productForm' => $productForm->createView()
+        ]);
+        // return $this->render('admin/products/index.html.twig');
     }
    
     #[Route('/delete/{id}', name: 'delete')]
