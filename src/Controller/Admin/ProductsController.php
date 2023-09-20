3988055761ee644697feebdb9fa832f40f528987
Derwych <seysmich@gmail.com>
2023-09-20 09:17:37 +0200

    admin without bundle

--- src/Controller/Admin/ProductsController.php
+++ src/Controller/Admin/ProductsController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Admin;
 use App\Entity\Images;
 use App\Entity\Products;
 use App\Form\ProductsFormType;
+use App\Repository\ProductsRepository;
 use App\Service\PictureService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -25,9 +26,10 @@ class ProductsController extends AbstractController
      * Summary of index
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function index(): Response
+    public function index(ProductsRepository $productsRepository): Response
     {
-        return $this->render('admin/products/index.html.twig');
+        $produits = $productsRepository->findAll();
+        return $this->render('admin/products/index.html.twig', compact('produits'));
     }
 
     #[Route('/add', name: 'add')]
